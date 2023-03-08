6c9fb2e1b538221c447f32bcbb0fce898c0aeed1
artemida <artemlenin96@gmail.com>
2023-03-08 22:49:35 +0500

    Sync Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -61,6 +61,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if (!IsOwner) return;
         HandleMovement();
         HandleInteractions();
     }
